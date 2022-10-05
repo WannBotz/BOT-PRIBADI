234b6982b7901db737f24cc933319e94d780d186
WannBotz <*mawangulo133@gmail.com*>
2022-10-06 02:09:24 +0700

    BOT-PRIBADI

---
+++ plugins/donasi.js
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+//let handler = async(m, { conn, text, usedPrefix, command }) => {
+//let pp = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image')
+
+let str = `${global.wm}
+
+┌─「 Donasi • Pulsa 」
+│ • *Telkomsel:* [${global.ppulsa}]
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+│ • *Dana:* [${global.pdana}]
+│ • *Gopay:* [${global.pgopay}]
+│ • *Ovo:* [${global.povo}]
+│ • *Link Aja:* [${global.plinkaja}]
+❏────`
+let wibu = `https://i.ibb.co/TqCZsdb/IMG-20221002-WA0035.jpg` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/wannzx_133",
+    mediaType: "VIDEO",
+    description: "https://www.instagram.com/reel/CfKCfcrAeA2/?igshid=YmMyMTA2M2Y=", 
+    title: 'Wannzx MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig
+  }
+  } }) 
+          }
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
---
+++ plugins/info-script.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*https://Instagram.com/wannzx_133*
+
+Pengen mentahan nya ?
+Nih ada santai bro....
+https://github.com/ImYanXiao/Elaina-MultiDevice
+
+Pengen yang udah di recode oleh owner?
+Script bot ini dijual bila kalian mau bisa chat owner
+
+wa.me/6281265970249`
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/wannzx_133",
+    mediaType: "VIDEO",
+    description: "https://www.instagram.com/reel/CfKCfcrAeA2/?igshid=YmMyMTA2M2Y=", 
+    title: 'Wannzx MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['source code']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+export default handler
